e944fed0da2dee8fba87e3611ad688f6e6cbddc8
John Liu <zodram@gmail.com>
2025-02-18 14:53:54 -0800

    fix: google api key

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com', 'i.postimg.cc']
   },
-  env: {
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-  },
 };
 
 export default nextConfig;
